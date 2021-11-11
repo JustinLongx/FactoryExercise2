78839f4f8452ced3ad8a8286419730f72dd5c18b
justin long <justinxlong93@gmail.com>
2021-11-11 15:33:59 -0600

    Initial commit

---
+++ FactoryExercise2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FactoryExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What database would you like to work with?");
+            Console.WriteLine("Type: sql");
+            Console.WriteLine("Type: mongo");
+            Console.WriteLine("Type: list");
+
+            var userInput = Console.ReadLine();
+
+            IDataAccess db = DataAccessFactory.GetDataAccessType(userInput);
+
+            var products = db.LoadData();
+            db.SaveData();
+
+            foreach (var item in products)
+            {
+                Console.WriteLine($"Name: {item.Name} Price:{item.Price}");
+            }
+        }
+    }
+}
